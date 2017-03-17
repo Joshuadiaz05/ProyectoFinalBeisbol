69b24cac9e4ae66f4b45a8ee7d4894d5566cb798
JUANDIII\JuanDiii <juandiegolopezve@gmail.com>
2017-03-16 21:55:06 -0400

    Nombre y posición atributos get & setter agregados

--- ProyectoFinalBesibol/src/logical/Jugador.java
+++ ProyectoFinalBesibol/src/logical/Jugador.java
@@ -2,10 +2,31 @@ package logical;
 
 public class Jugador {
 	private String nombre;
+	private String posicion;
 	
 
 	public Jugador() {
 		// TODO Auto-generated constructor stub
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getPosicion() {
+		return posicion;
+	}
+
+
+	public void setPosicion(String posicion) {
+		this.posicion = posicion;
 	}
 
 }
