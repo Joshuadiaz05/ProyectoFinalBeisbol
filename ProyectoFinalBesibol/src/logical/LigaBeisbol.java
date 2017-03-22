0e3bf8ec84d78ca590e912a1335258346b5242a4
Carlo <Carlo@Carlos>
2017-03-22 16:45:23 -0400

    Clase Liga y partido

--- ProyectoFinalBesibol/src/logical/LigaBeisbol.java
+++ ProyectoFinalBesibol/src/logical/LigaBeisbol.java
@@ -7,5 +7,29 @@ public class LigaBeisbol {
 	private ArrayList<Equipos> equipo;
 	private ArrayList<Jugadores> jugador;
 	private ArrayList<Partido> partido;
+	public LigaBeisbol(ArrayList<Equipos> equipo, ArrayList<Jugadores> jugador, ArrayList<Partido> partido) {
+		super();
+		this.equipo = equipo;
+		this.jugador = jugador;
+		this.partido = partido;
+	}
+	public ArrayList<Equipos> getEquipo() {
+		return equipo;
+	}
+	public void setEquipo(ArrayList<Equipos> equipo) {
+		this.equipo = equipo;
+	}
+	public ArrayList<Jugadores> getJugador() {
+		return jugador;
+	}
+	public void setJugador(ArrayList<Jugadores> jugador) {
+		this.jugador = jugador;
+	}
+	public ArrayList<Partido> getPartido() {
+		return partido;
+	}
+	public void setPartido(ArrayList<Partido> partido) {
+		this.partido = partido;
+	}
 
 }
--- ProyectoFinalBesibol/src/logical/Partido.java
+++ ProyectoFinalBesibol/src/logical/Partido.java
@@ -11,6 +11,59 @@ public class Partido {
 	private String hora;
 	private int carrerasCasa;
 	private int carrerasVisita;
+	public Partido(Date fecha, String equipoCasa, String equipoVisita, String estadio, String hora, int carrerasCasa,
+			int carrerasVisita) {
+		super();
+		this.fecha = fecha;
+		this.equipoCasa = equipoCasa;
+		this.equipoVisita = equipoVisita;
+		Estadio = estadio;
+		this.hora = hora;
+		this.carrerasCasa = carrerasCasa;
+		this.carrerasVisita = carrerasVisita;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public String getEquipoCasa() {
+		return equipoCasa;
+	}
+	public void setEquipoCasa(String equipoCasa) {
+		this.equipoCasa = equipoCasa;
+	}
+	public String getEquipoVisita() {
+		return equipoVisita;
+	}
+	public void setEquipoVisita(String equipoVisita) {
+		this.equipoVisita = equipoVisita;
+	}
+	public String getEstadio() {
+		return Estadio;
+	}
+	public void setEstadio(String estadio) {
+		Estadio = estadio;
+	}
+	public String getHora() {
+		return hora;
+	}
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+	public int getCarrerasCasa() {
+		return carrerasCasa;
+	}
+	public void setCarrerasCasa(int carrerasCasa) {
+		this.carrerasCasa = carrerasCasa;
+	}
+	public int getCarrerasVisita() {
+		return carrerasVisita;
+	}
+	public void setCarrerasVisita(int carrerasVisita) {
+		this.carrerasVisita = carrerasVisita;
+	}
 	
 	
 }
